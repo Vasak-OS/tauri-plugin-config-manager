24203bb58389cd6c3558108b7e1c2b4f816167c6
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-06-05 14:58:19 -0300

    Refactor funciones de configuración; reemplazar ping con writeConfig y mejorar la estructura del código

--- guest-js/index.ts
+++ guest-js/index.ts
@@ -1,9 +1,23 @@
-import { invoke } from '@tauri-apps/api/core'
+import { invoke } from "@tauri-apps/api/core";
 
-export async function ping(value: string): Promise<string | null> {
-  return await invoke<{value?: string}>('plugin:config-manager|ping', {
+export async function writeConfig(value: VSKConfig): Promise<void> {
+  await invoke<{ value?: string }>("plugin:config-manager|write_config", {
     payload: {
       value,
     },
-  }).then((r) => (r.value ? r.value : null));
+  });
 }
+
+export async function readConfig(): Promise<string | null> {
+  return await invoke<{ value?: string }>(
+    "plugin:config-manager|read_config"
+  ).then((r) => (r.value ? r.value : null));
+}
+
+export type VSKConfig = {
+  style: {
+    darkMode: boolean;
+    primaryColor: string;
+    borderRadius: number;
+  };
+};
