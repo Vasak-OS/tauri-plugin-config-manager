c6d0f0022541e6af5f43f621717fd6d93543e655
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-06-11 14:54:43 -0300

    Agregar store de configuración utilizando Pinia; implementar carga y gestión de propiedades de configuración

--- guest-js/index.ts
+++ guest-js/index.ts
@@ -1,4 +1,6 @@
 import { invoke } from "@tauri-apps/api/core";
+import { defineStore } from "pinia";
+import { ref } from "vue";
 
 export async function writeConfig(value: VSKConfig): Promise<void> {
   await invoke("plugin:config-manager|write_config", {
@@ -32,3 +34,45 @@ export type VSKConfig = {
     logo: string;
   };
 };
+
+export const useConfigStore = defineStore("config", () => {
+  const config = ref<VSKConfig | null>(null);
+
+  const loadConfig = async () => {
+    config.value = await readConfig();
+    if (config.value === null) {
+      console.error("Failed to load configuration");
+      return;
+    }
+    setMode();
+    setProperties();
+  };
+
+  const setMode = () => {
+    if (config.value?.style.darkmode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  };
+
+  const setProperties = () => {
+    if (config.value) {
+      document.documentElement.style.setProperty(
+        "--primary-color",
+        config.value.style.color || "#4a90e2"
+      );
+      document.documentElement.style.setProperty(
+        "--border-radius",
+        `${config.value.style.radius}px` || "8px"
+      );
+    }
+  };
+
+  return {
+    config,
+    loadConfig,
+    setMode,
+    setProperties,
+  };
+});
